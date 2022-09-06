5ddd301abed9a3897a556279d8e31961f4ed573a
AAdam-P <61825850+AAdam-P@users.noreply.github.com>
2022-09-06 23:53:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+    include_once("../MoonLabs/module/global/variables.php");
+    include_once("../MoonLabs/module/global/functions.php");
+    include_once("../MoonLabs/module/global/controller.php");
+?>
